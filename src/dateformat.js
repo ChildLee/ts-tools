d9d2b30c2e4f2b56974d85e6a24016d3480a30bf
lxzgg <471227142@qq.com>
2018-08-29 20:32:27 +0800

    修改dateformat.js

--- src/dateformat.js
+++ src/dateformat.js
@@ -1,89 +1,87 @@
-;(function (global) {
-    function dateformat(date, mask) {
-
-        if (arguments.length === 1 && Object.prototype.toString.call(date) === '[object String]' && !/\d/.test(date)) {
-            mask = date
-            date = undefined
-        }
-
-        date = date || new Date()
-
-        if (!(date instanceof Date)) {
-            date = new Date(date)
-        }
-
-        if (isNaN(date)) {
-            throw TypeError('Format Date Error')
-        }
-
-        mask = String(dateformat.masks[mask] || mask || dateformat.masks['default'])
-
-        var arr = [{
-            reg: /(Y+)/,
-            replace: date.getFullYear()
-        }, {
-            reg: /(M+)/,
-            replace: date.getMonth() + 1
-        }, {
-            reg: /(D+)/,
-            replace: date.getDate()
-        }, {
-            reg: /(H+)/,
-            replace: date.getHours()
-        }, {
-            reg: /(m+)/,
-            replace: date.getMinutes()
-        }, {
-            reg: /(s+)/,
-            replace: date.getSeconds()
-        }, {
-            reg: /(S+)/,
-            replace: date.getMilliseconds()
-        }]
-
-        var len = arr.length - 1
-
-        //处理年份
-        arr[0].reg.test(mask) ?
-            mask = mask.replace(RegExp.$1, String(arr[0].replace).substring(4 - RegExp.$1.length)) : null
-        //处理月日时分秒
-        for (var i = 1; i < len; i++) {
-            arr[i].reg.test(mask) ?
-                RegExp.$1.length >= 2 ?
-                    mask = mask.replace(RegExp.$1, ('00' + arr[i].replace).substring(String(arr[i].replace).length))
-                    : mask = mask.replace(RegExp.$1, String(arr[i].replace))
-                : null
-        }
-        //处理毫秒
-        arr[len].reg.test(mask) ?
-            mask = mask.replace(RegExp.$1, ('000' + arr[len].replace).substring(String(arr[len].replace).length).substring(3 - RegExp.$1.length))
-            : null
-
-        return mask
+/**
+ * 格式化时间
+ * dateFormat()                           //2018-08-08 08:08:08
+ * dateFormat('YYYY-MM-DD HH:mm:ss SSS')  //2018-08-08 08:08:08 888
+ * dateFormat('YY-M-D H:m:s')             //18-8-8 8:8:8
+ * dateFormat('default')                  //2018-08-08 08:08:08
+ * dateFormat('YYYY-MM-DD')               //2018-08-08
+ * dateFormat('date')                     //2018-08-08
+ * dateFormat('HH:mm:ss')                 //08:08:08
+ * dateFormat('time')                     //08:08:08
+ * dateFormat(new Date())                 //2018-08-08 08:08:08
+ * dateFormat(Date.now())                 //2018-08-08 08:08:08
+ */
+function dateFormat(date, mask) {
+    if (arguments.length === 1 && Object.prototype.toString.call(date) === '[object String]' && !/\d/.test(date)) {
+        mask = date
+        date = undefined
     }
 
-    dateformat.masks = {
-        'default': 'YYYY-MM-DD HH:mm:ss',
-        'date': 'YYYY-MM-DD',
-        'time': 'HH:mm:ss'
+    date = date || new Date()
+
+    if (!(date instanceof Date)) {
+        date = new Date(date)
     }
 
-    global.dateformat = dateformat
+    if (isNaN(date)) {
+        throw TypeError('Format Date Error')
+    }
 
-})(typeof window !== 'undefined' ? window : global)
+    mask = String(dateFormat.masks[mask] || mask || dateFormat.masks['default'])
 
-console.log(dateformat())
-console.log(dateformat(Date.now()))
-console.log(dateformat(1529214419207))
-console.log(dateformat(Date.now(), 'YMDHms'))
-console.log(dateformat('YYYY-MM-DD HH:mm:ss SSS'))
-console.log(dateformat('YYYY年MM月DD日 HH时mm分ss秒 SSS毫秒'))
-console.log(dateformat('date'))
-console.log(dateformat('time'))
-// setInterval(function () {
-//     console.time('1')
-//     console.log(dateformat('YYYY年MM月DD日 HH时mm分ss秒'))
-//     console.timeEnd('1')
-// }, 1000)
+    let arr = [{
+        reg: /(Y+)/,
+        replace: date.getFullYear()
+    }, {
+        reg: /(M+)/,
+        replace: date.getMonth() + 1
+    }, {
+        reg: /(D+)/,
+        replace: date.getDate()
+    }, {
+        reg: /(H+)/,
+        replace: date.getHours()
+    }, {
+        reg: /(m+)/,
+        replace: date.getMinutes()
+    }, {
+        reg: /(s+)/,
+        replace: date.getSeconds()
+    }, {
+        reg: /(S+)/,
+        replace: date.getMilliseconds()
+    }]
 
-// export default dateformat
+    let len = arr.length - 1
+    let replace
+    //处理年份
+    if (arr[0].reg.test(mask)) {
+        replace = arr[0].replace.toString()
+        mask = mask.replace(RegExp.$1, replace.substring(4 - RegExp.$1.length))
+    }
+    //处理月日时分秒
+    for (let i = 1; i < len; i++) {
+        if (arr[i].reg.test(mask)) {
+            replace = arr[i].replace.toString()
+            if (RegExp.$1.length >= 2) {
+                mask = mask.replace(RegExp.$1, ('00' + replace).substring(replace.length))
+            } else {
+                mask = mask.replace(RegExp.$1, replace)
+            }
+        }
+    }
+    //处理毫秒
+    if (arr[len].reg.test(mask)) {
+        replace = arr[len].replace.toString()
+        mask = mask.replace(RegExp.$1, ('000' + replace).substring(replace.length).substring(3 - RegExp.$1.length))
+    }
+    return mask
+}
+
+dateFormat.masks = {
+    'default': 'YYYY-MM-DD HH:mm:ss',
+    'date': 'YYYY-MM-DD',
+    'time': 'HH:mm:ss'
+}
+
+export default dateFormat
