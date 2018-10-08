aff2011b95a1e5f2f3c7e8caf9e6755899cccc83
lxzgg <471227142@qq.com>
2018-10-08 18:22:46 +0800

    update hash

---
+++ src/hash.js
@@ -0,0 +1,11 @@
+const bcrypt = require('bcryptjs')
+
+it('should hash', function () {
+    console.log(bcrypt.hashSync('1'))
+})
+
+it('should ha', function () {
+    bcrypt.compare('1', '$2a$10$HOsfIaPNhAj3fhmAy5yO9.jmWKSu/wa6Iee4CvJI4QccVS43u.yh.').then(res => {
+        console.log(res)
+    })
+}) 
